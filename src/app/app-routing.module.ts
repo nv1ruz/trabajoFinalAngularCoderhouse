85c64c859230a9d01c070f3c285fc9fe9c0cb163
Braian Martinez <braian.martz@gmail.com>
2021-12-09 19:26:05 -0300

    add nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AutenticacionComponent } from './pages/autenticacion/autenticacion.component';
+import { PeliculaDetalleComponent } from './pages/pelicula-detalle/pelicula-detalle.component';
+import { PeliculasListadoComponent } from './pages/peliculas-listado/peliculas-listado.component';
 
 
 const routes: Routes = [
 
-	{ path: '', component: HomeComponent },
 	{ path: 'ingresar', component: AutenticacionComponent },
+	{ 
+		path: '', component: HomeComponent,
+		children: [
+			{ path: 'peliculas', component: PeliculasListadoComponent },
+			{ path: 'peliculas/:pelicula_id', component: PeliculaDetalleComponent },
+			{ path: '', pathMatch: 'full', redirectTo: 'peliculas' }
+		]
+	},
 	{ path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 
