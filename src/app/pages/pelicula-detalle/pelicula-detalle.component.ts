93e40f913012f20d9ebee2446e2a08c7d88c8861
Braian Martinez <braian.martz@gmail.com>
2021-12-09 19:53:04 -0300

    add pelicula detalle

--- src/app/pages/pelicula-detalle/pelicula-detalle.component.ts
+++ src/app/pages/pelicula-detalle/pelicula-detalle.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { IPelicula } from 'src/app/data/interfaces/peliculas.interface';
+import { PeliculasApiService } from 'src/app/data/services/peliculas-api.service';
 
 @Component({
-  selector: 'app-pelicula-detalle',
-  templateUrl: './pelicula-detalle.component.html',
-  styleUrls: ['./pelicula-detalle.component.css']
+	selector: 'app-pelicula-detalle',
+	templateUrl: './pelicula-detalle.component.html',
+	styleUrls: ['./pelicula-detalle.component.css']
 })
 export class PeliculaDetalleComponent implements OnInit {
+	private peliculaId?: number;
+	public pelicula?: IPelicula;
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private _peliculasApi: PeliculasApiService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.obtenerParametro();
+	}
 
+
+	private obtenerParametro(): void
+	{
+		this.route.paramMap.subscribe(
+			(params: ParamMap) => {
+				this.peliculaId = +params.get('pelicula_id')!;
+				this.cargarPelicula(this.peliculaId);
+			}
+		);
+	}
+
+
+	private cargarPelicula(peliculaId: number): void
+	{
+		const response = this._peliculasApi.obtenerPeliculaPorId(peliculaId);
+		if(response){
+			this.pelicula = response;
+		} else{
+			console.log('La pelicula no existe');
+		}
+	}
+
+
+	public volver(): void
+	{
+		this.router.navigateByUrl('peliculas');
+	}
 }
