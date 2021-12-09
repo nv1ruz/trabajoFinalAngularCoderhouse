07e64517c95d5e417409ea86c3d3b0ff55eb3839
Braian Martinez <braian.martz@gmail.com>
2021-12-09 18:23:38 -0300

    add registro

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+	selector: 'app-registro',
+	templateUrl: './registro.component.html',
+	styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent implements OnInit {
+	private isEmail: RegExp = /^\S+@\S+\.\S+$/;
+	public formularioRegistrar: FormGroup = this.fb.group(
+		{
+			email: new FormControl('', [Validators.required, Validators.pattern(this.isEmail)]),
+			password: new FormControl('', [Validators.required]),
+			password2: new FormControl('', [Validators.required]),
+		}
+	);
+	public isWrongPassword: boolean = false;
 
-  constructor() { }
+	constructor(
+		private fb: FormBuilder
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	// 
+
+	onSubmit(): void
+	{
+		console.log('Formulario registrar: ', this.formularioRegistrar.value);
+	}
+
+	handlerPassword(): void
+	{
+		const password = this.formularioRegistrar.controls['password'].value;
+		const password2 = this.formularioRegistrar.controls['password2'].value;
+		
+		this.isWrongPassword = (password !== password2)? true: false;
+	}
 
 }
