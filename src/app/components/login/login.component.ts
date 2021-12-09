0c45c9533c037a32d2155d6c90db0b4d05ba3aa5
Braian Martinez <braian.martz@gmail.com>
2021-12-09 18:12:30 -0300

    add formulario login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	private isEmail: RegExp = /^\S+@\S+\.\S+$/;
+	public formularioLogin: FormGroup = this.fb.group(
+		{
+			email: new FormControl('', [Validators.required, Validators.pattern(this.isEmail)]),
+			password: new FormControl('', [Validators.required]),
+		}
+	);
 
-  constructor() { }
+	constructor(
+		private fb: FormBuilder,
+		private router: Router
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	// 
+
+	onSubmit(): void
+	{
+		console.log('Formulario login: ', this.formularioLogin.value);
+		this.router.navigateByUrl('home');
+	}
 
 }
