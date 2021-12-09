40b9ae58963802a8d29a38f265164fbafc865cbf
Braian Martinez <braian.martz@gmail.com>
2021-12-09 20:28:57 -0300

    add funcionalidad a carrito component

---
+++ src/app/pages/carrito/carrito.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CarritoService } from 'src/app/services/carrito.service';
+
+@Component({
+  selector: 'app-carrito',
+  templateUrl: './carrito.component.html',
+  styleUrls: ['./carrito.component.css']
+})
+export class CarritoComponent implements OnInit {
+
+  constructor(
+    public _carritoService: CarritoService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
