74560c5b21618894316a0e6e81760e612fe3d7ed
Braian Martinez <braian.martz@gmail.com>
2021-12-08 22:45:57 -0300

    add pagina autenticacion

--- src/app/pages/autenticacion/autenticacion.component.ts
+++ src/app/pages/autenticacion/autenticacion.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-autenticacion',
-  templateUrl: './autenticacion.component.html',
-  styleUrls: ['./autenticacion.component.css']
+	selector: 'app-autenticacion',
+	templateUrl: './autenticacion.component.html',
+	styleUrls: ['./autenticacion.component.css']
 })
 export class AutenticacionComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
