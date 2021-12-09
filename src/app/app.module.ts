5cbf2a37416b1d6403136e493a355f615a0721dd
Braian Martinez <braian.martz@gmail.com>
2021-12-09 18:12:12 -0300

    add ReactiveForms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './modules/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { AutenticacionComponent } from './pages/autenticacion/autenticacion.component';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { AutenticacionComponent } from './pages/autenticacion/autenticacion.comp
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     SharedModule
   ],
   providers: [],
