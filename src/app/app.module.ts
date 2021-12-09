b2da656f4ebe7c4dff8088e8a00e5abbc87b2639
Braian Martinez <braian.martz@gmail.com>
2021-12-09 18:41:36 -0300

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './modules/shared/shared.module';
@@ -24,7 +25,8 @@ import { AutenticacionComponent } from './pages/autenticacion/autenticacion.comp
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
