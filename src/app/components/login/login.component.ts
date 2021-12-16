a095e065102e7636c9c3769e1a4191a08616c833
Braian Martinez <braian.martz@gmail.com>
2021-12-16 20:18:35 -0300

    se inicializa formulario login desde  ngOnInit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,36 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
-
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-	private isEmail: RegExp = /^\S+@\S+\.\S+$/;
-	public formularioLogin: FormGroup = this.fb.group(
-		{
-			email: new FormControl('', [Validators.required, Validators.pattern(this.isEmail)]),
-			password: new FormControl('', [Validators.required]),
-		}
-	);
+  private isEmail: RegExp = /^\S+@\S+\.\S+$/;
+  public formularioLogin!: FormGroup;
 
-	constructor(
-		private fb: FormBuilder,
-		private router: Router
-	) { }
+  constructor(private fb: FormBuilder, private router: Router) {}
 
-	ngOnInit(): void {
-	}
+  ngOnInit(): void {
+    this.inicializarFormularioLogin();
+  }
 
-	// 
+  //
 
-	onSubmit(): void
-	{
-		console.log('Formulario login: ', this.formularioLogin.value);
-		this.router.navigateByUrl('');
-	}
+  private inicializarFormularioLogin(): void {
+    this.formularioLogin = this.fb.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.isEmail),
+      ]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
 
+  onSubmit(): void {
+    console.log('Formulario login: ', this.formularioLogin.value);
+    this.router.navigateByUrl('');
+  }
 }
