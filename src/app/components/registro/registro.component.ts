be163ce05eb94c935d53cafad435762edb04622f
Braian Martinez <braian.martz@gmail.com>
2021-12-16 20:20:13 -0300

    se inicializa formulario registrar desde ngOnInit

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,42 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-	selector: 'app-registro',
-	templateUrl: './registro.component.html',
-	styleUrls: ['./registro.component.css']
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
-	private isEmail: RegExp = /^\S+@\S+\.\S+$/;
-	public formularioRegistrar: FormGroup = this.fb.group(
-		{
-			email: new FormControl('', [Validators.required, Validators.pattern(this.isEmail)]),
-			password: new FormControl('', [Validators.required]),
-			password2: new FormControl('', [Validators.required]),
-		}
-	);
-	public isWrongPassword: boolean = false;
+  private isEmail: RegExp = /^\S+@\S+\.\S+$/;
+  public formularioRegistrar!: FormGroup;
+  public isWrongPassword: boolean = false;
 
-	constructor(
-		private fb: FormBuilder
-	) { }
+  constructor(private fb: FormBuilder) {}
 
-	ngOnInit(): void {
-	}
+  ngOnInit(): void {
+    this.inicializarFormularioRegistrar();
+  }
 
-	// 
+  //
 
-	onSubmit(): void
-	{
-		console.log('Formulario registrar: ', this.formularioRegistrar.value);
-	}
+  private inicializarFormularioRegistrar(): void {
+    this.formularioRegistrar = this.fb.group({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(this.isEmail),
+      ]),
+      password: new FormControl('', [Validators.required]),
+      password2: new FormControl('', [Validators.required]),
+    });
+  }
 
-	handlerPassword(): void
-	{
-		const password = this.formularioRegistrar.controls['password'].value;
-		const password2 = this.formularioRegistrar.controls['password2'].value;
-		
-		this.isWrongPassword = (password !== password2)? true: false;
-	}
+  onSubmit(): void {
+    console.log('Formulario registrar: ', this.formularioRegistrar.value);
+  }
 
+  handlerPassword(): void {
+    const password = this.formularioRegistrar.controls['password'].value;
+    const password2 = this.formularioRegistrar.controls['password2'].value;
+
+    this.isWrongPassword = password !== password2 ? true : false;
+  }
 }
