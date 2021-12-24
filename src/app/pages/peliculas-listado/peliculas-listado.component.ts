deda37a06ca3dbb13a2cf5628f8f3a4641c2c2eb
nv1ruz <braian.martz@gmail.com>
2021-12-24 17:43:56 -0300

    componente sin uso

--- src/app/pages/peliculas-listado/peliculas-listado.component.ts
+++ src/app/pages/peliculas-listado/peliculas-listado.component.ts
@@ -5,66 +5,58 @@ import { IPelicula } from 'src/app/data/interfaces/peliculas.interface';
 import { PeliculasApiService } from 'src/app/data/services/peliculas-api.service';
 import { CarritoService } from 'src/app/services/carrito.service';
 
-
 @Component({
-	selector: 'app-peliculas-listado',
-	templateUrl: './peliculas-listado.component.html',
-	styleUrls: ['./peliculas-listado.component.css']
+    selector: 'app-peliculas-listado',
+    templateUrl: './peliculas-listado.component.html',
+    styleUrls: ['./peliculas-listado.component.css'],
 })
 export class PeliculasListadoComponent implements OnInit {
-	public peliculas: IPelicula[] = [];
+    public peliculas: IPelicula[] = [];
 
-	constructor(
-		private _peliculasApi: PeliculasApiService,
-		private router: Router,
-		private _carrito: CarritoService
-	) { }
+    constructor(
+        private _peliculasApi: PeliculasApiService,
+        private router: Router,
+        private _carrito: CarritoService
+    ) {}
 
-	ngOnInit(): void {
-		this.cargarPeliculas();
-	}
+    ngOnInit(): void {
+        this.cargarPeliculas();
+    }
 
+    private cargarPeliculas(): void {
+        this.peliculas = this._peliculasApi.obtenerPeliculas();
+    }
 
+    public verDetallePelicula(peliculaId: number): void {
+        this.router.navigateByUrl(`/peliculas/${peliculaId}`);
+    }
 
-	private cargarPeliculas(): void
-	{
-		this.peliculas = this._peliculasApi.obtenerPeliculas();
-	}
+    // public agregarProducto(item: IPelicula): void
+    // {
+    // 	const producto = this._carrito.obtenerProducto(item.id);
+    // 	if(!producto){
+    // 		this._carrito.agregarProducto(item);
+    // 	} else{
+    // 		console.log('El producto ya está en el carrito!!');
+    // 	}
+    // }
 
-	
-	public verDetallePelicula(peliculaId: number): void
-	{
-		this.router.navigateByUrl(`/peliculas/${peliculaId}`);
-	}
+    // public quitarProducto(item: IPelicula): void
+    // {
+    // 	const producto = this._carrito.obtenerProducto(item.id);
+    // 	if(producto){
+    // 		this._carrito.quitarProducto(item);
+    // 	} else{
+    // 		console.log('El producto no está en el carrito!!');
+    // 	}
+    // }
 
-
-	public agregarProducto(item: IPelicula): void
-	{
-		const producto = this._carrito.obtenerProducto(item.id);
-		if(!producto){
-			this._carrito.agregarProducto(item);
-		} else{
-			console.log('El producto ya está en el carrito!!');
-		}
-	}
-
-	public quitarProducto(item: IPelicula): void
-	{
-		const producto = this._carrito.obtenerProducto(item.id);
-		if(producto){
-			this._carrito.quitarProducto(item);
-		} else{
-			console.log('El producto no está en el carrito!!');
-		}
-	}
-
-
-	public verificarProductoEnCarrito(productoId: number): boolean
-	{
-		let resultado: boolean = false;
-		if(this._carrito.carrito.productos.find(item => item.id == productoId)){
-			return true;
-		}
-		return resultado;
-	}
+    // public verificarProductoEnCarrito(productoId: number): boolean
+    // {
+    // 	let resultado: boolean = false;
+    // 	if(this._carrito.carrito.productos.find(item => item.id == productoId)){
+    // 		return true;
+    // 	}
+    // 	return resultado;
+    // }
 }
