34289f1362e533795a95018856819b7b484711dc
nv1ruz <braian.martz@gmail.com>
2021-12-24 17:43:30 -0300

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,33 +3,37 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AutenticacionComponent } from './pages/autenticacion/autenticacion.component';
-import { PeliculaDetalleComponent } from './pages/pelicula-detalle/pelicula-detalle.component';
-import { PeliculasListadoComponent } from './pages/peliculas-listado/peliculas-listado.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
+import { MoviesListComponent } from './pages/movies-list/movies-list.component';
+import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+// import { PeliculaDetalleComponent } from './pages/pelicula-detalle/pelicula-detalle.component';
+// import { PeliculasListadoComponent } from './pages/peliculas-listado/peliculas-listado.component';
 
 const routes: Routes = [
-  { path: 'ingresar', component: AutenticacionComponent },
-  {
-    path: '',
-    component: HomeComponent,
-    children: [
-      { path: 'peliculas', component: PeliculasListadoComponent },
-      { path: 'peliculas/:pelicula_id', component: PeliculaDetalleComponent },
-      { path: 'carrito', component: CarritoComponent },
-      {
-        path: 'admin',
-        loadChildren: () =>
-          import('./modules/admin/admin.module').then((m) => m.AdminModule),
-      },
-      { path: '', pathMatch: 'full', redirectTo: 'peliculas' },
-    ],
-  },
-  { path: '**', pathMatch: 'full', redirectTo: '' },
+    { path: 'ingresar', component: AutenticacionComponent },
+    {
+        path: '',
+        component: HomeComponent,
+        children: [
+            // { path: 'peliculas', component: PeliculasListadoComponent },
+            // { path: 'peliculas/:pelicula_id', component: PeliculaDetalleComponent },
+            { path: 'movies', component: MoviesListComponent },
+            { path: 'movies/:movieId', component: MovieDetailComponent },
+            { path: 'cart', component: CarritoComponent },
+            {
+                path: 'admin',
+                loadChildren: () =>
+                    import('./modules/admin/admin.module').then((m) => m.AdminModule),
+            },
+            { path: '', pathMatch: 'full', redirectTo: 'movies' },
+        ],
+    },
+    { path: '**', pathMatch: 'full', redirectTo: '' },
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
