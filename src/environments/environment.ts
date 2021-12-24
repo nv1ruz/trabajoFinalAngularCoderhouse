52ded852db088c9024552ce4497f25b8e4ab8a10
nv1ruz <braian.martz@gmail.com>
2021-12-24 13:18:34 -0300

    add url base de api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  base_url_api_peliculas: 'http://61b92fc138f69a0017ce5ef8.mockapi.io/api/v1',
 };
 
 /*
