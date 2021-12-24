17c8b61db4dddd3edff30d412f5f2b899b2f4f6d
nv1ruz <braian.martz@gmail.com>
2021-12-24 13:19:25 -0300

    add user interface

---
+++ src/app/data/interfaces/users-api.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  id: string;
+  avatar: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  role: string;
+  created_at: string;
+  updated_at: string;
+}
