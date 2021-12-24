136ddb9587d93160cdefd72cec6b2f086ce24a3e
nv1ruz <braian.martz@gmail.com>
2021-12-24 17:44:03 -0300

    fixes

--- src/app/modules/shared/components/navbar/navbar.component.ts
+++ src/app/modules/shared/components/navbar/navbar.component.ts
@@ -2,27 +2,22 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-	selector: 'app-navbar',
-	templateUrl: './navbar.component.html',
-	styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-	@Input() title: string = 'Coderhouse';
+    @Input() title: string = 'Coderhouse';
 
-	constructor(
-		private router: Router
-	) { }
+    constructor(private router: Router) {}
 
-	ngOnInit(): void {
-	}
+    ngOnInit(): void {}
 
-	irHome(): void
-	{
-		this.router.navigateByUrl('');
-	}
+    irHome(): void {
+        this.router.navigateByUrl('');
+    }
 
-	irCarrito(): void
-	{
-		this.router.navigateByUrl('carrito');
-	}
+    irCarrito(): void {
+        this.router.navigateByUrl('cart');
+    }
 }
--- src/app/services/carrito.service.ts
+++ src/app/services/carrito.service.ts
@@ -1,42 +1,40 @@
 import { Injectable } from '@angular/core';
+import { IMovie } from '../data/interfaces/movies-api.interface';
 import { IPelicula } from '../data/interfaces/peliculas.interface';
 
-export interface ICarrito{
-	productos: IPelicula[];
-	direccion_envio: string;
+export interface ICarrito {
+    productos: IMovie[];
+    direccion_envio: string;
 }
 
 @Injectable({
-	providedIn: 'root'
+    providedIn: 'root',
 })
 export class CarritoService {
-	private _carrito: ICarrito = {
-		productos: [],
-		direccion_envio: '',
-	};
+    private _carrito: ICarrito = {
+        productos: [],
+        direccion_envio: '',
+    };
 
+    constructor() {}
 
-	constructor() { }
+    get carrito(): ICarrito {
+        return this._carrito;
+    }
 
-	get carrito(): ICarrito{
-		return this._carrito;
-	}
+    public agregarProducto(item: IMovie): void {
+        this._carrito.productos.push(item);
+        console.log(this._carrito.productos);
+    }
 
+    public quitarProducto(item: IMovie): void {
+        this._carrito.productos = this._carrito.productos.filter(
+            (producto) => producto.id !== item.id
+        );
+        console.log(this._carrito.productos);
+    }
 
-	public agregarProducto(item: IPelicula): void
-	{
-		this._carrito.productos.push(item);
-		console.log(this._carrito.productos);
-	}
-
-	public quitarProducto(item: IPelicula): void
-	{
-		this._carrito.productos = this._carrito.productos.filter( producto => producto.id !== item.id );
-		console.log(this._carrito.productos);
-	}
-
-	public obtenerProducto(itemId: number): IPelicula | undefined
-	{
-		return this._carrito.productos.find(item => item.id == itemId);
-	}
+    public obtenerProducto(itemId: string): IMovie | undefined {
+        return this._carrito.productos.find((item) => item.id == itemId);
+    }
 }
